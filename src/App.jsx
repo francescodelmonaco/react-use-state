d2c3d3e1a888ea9b2199f4c689ed14c8f591b730
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-21 14:43:09 +0100

    add: header component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
     </>
   )
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1 className="margin-80">Learn Web Development</h1>
+        </header>
+    )
+};
+
+export default Header;
