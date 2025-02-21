9aff58402cc7552f82f531ef6e36c2d6cabe2d8a
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-21 15:15:19 +0100

    add: button list component and style

---
+++ src/components/ButtonList.jsx
@@ -0,0 +1,14 @@
+const ButtonList = () => {
+    return (
+        <div className="margin-60 button-list">
+            <button className="active">HTML</button>
+            <button className="">CSS</button>
+            <button className="">JavaScript</button>
+            <button className="">Node.js</button>
+            <button className="">Express</button>
+            <button className="">ReactJS</button>
+        </div>
+    )
+};
+
+export default ButtonList;
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import ButtonList from "./ButtonList";
+import Card from "./Card";
+
+const Main = () => {
+    return (
+        <main>
+            <ButtonList />
+            <Card />
+        </main>
+    )
+};
+
+export default Main;
